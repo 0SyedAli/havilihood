11fbc01279706138639ba5ce29de85507b46778a
SyedAli002 <developerweb.ahtisham@gmail.com>
2024-11-05 21:02:48 +0500

    add radio button

--- signup.php
+++ signup.php
@@ -11,28 +11,14 @@ include 'includes/header.php';
             <h2>Signup</h2>
             <p>Enter your details</p>
             <form action="javascript:;">
-                <div class="radio_dash d-flex align-items-center justify-content-between">
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-                        <label class="form-check-label" for="flexRadioDefault1">
-                            As Job Seeker
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
-                        <label class="form-check-label" for="flexRadioDefault2">
-                            As Recruiter
-                        </label>
-                    </div>
-                </div>
                 <div class="d-flex align-items-center">
                     <div class="w25 px-0 d-flex align-items-center gap-2">
                         <div class="custom-radio">
                             <input class="form-check-input" type="radio" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
                             <div class="checkmark"></div>
                         </div>
-                        <label class="form-check-label" for="check12">
-                            Must have qualification
+                        <label class="form-check-label" for="flexRadioDefault1">
+                            As Job Seeker
                         </label>
                     </div>
                     <div class="w25 px-0 d-flex align-items-center gap-2">
@@ -40,8 +26,8 @@ include 'includes/header.php';
                             <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
                             <div class="checkmark"></div>
                         </div>
-                        <label class="form-check-label" for="check12">
-                            Must have qualification
+                        <label class="form-check-label" for="flexRadioDefault2">
+                            As Recruiter
                         </label>
                     </div>
                 </div>
